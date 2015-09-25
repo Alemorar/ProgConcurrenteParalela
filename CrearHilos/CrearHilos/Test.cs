75a43f1a5ddb023611447768ba32a488474df06c
Alemorar <infoserv100@gmail.com>
2015-09-25 02:31:32 -0300

    agregamos proyectos

---
+++ CrearHilos/CrearHilos/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Test
+{
+
+    //Hilo primario
+    public static void Main()
+    {
+        CHilo hiloSecundario = new CHilo();
+        //Esperar a que el hilo secundario finalice.
+        hiloSecundario.HiloSubyacente.Join();
+    }
+}
+
+
---
+++ TrabajoPractico2/TrabajoPractico2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+/////////////////////////////////////////
+// Hilos.
+/////////////////////////////////////////
+public class CHilo
+{
+    //Clase que define un hilo secundario
+    private Thread hilo; // identificador del hilo
+    private int nMuestras;
+
+    public CHilo()
+        : this(null)
+    {
+    }
+
+    public CHilo(string nombreHilo)
+    {
+        nMuestras = 0;
+        //Crear el hilo identificado por hilo
+        hilo = new Thread(ProcHilo);
+        if (nombreHilo != null) hilo.Name = nombreHilo;
+    }
+
+    public Thread HiloSubyacente//proceso thread
+    {
+        get { return hilo; } //referencia al objeto Thread
+    }
+
+    private void ProcHilo(Object datos)
+    {
+        nMuestras = NRandom((int)datos);
+        for (int i = 0; i < nMuestras; i++)
+        {
+            Console.WriteLine(hilo.Name + " tomando muestra " + (i + 1));
+            Esperar(500); //ticks
+        }
+    }
+
+    private int NRandom(int n)
+    {
+        Random rnd = new Random();
+        int limiteSup = n, limiteInf = 1;
+        return (int)((limiteSup - limiteInf + 1) * rnd.NextDouble() +
+            limiteInf); // valor entre 1 y n
+    }
+
+    private void Esperar(int n)
+    {
+        long tmi = DateTime.Now.Ticks + n;//ticks
+        while (DateTime.Now.Ticks < tmi) ;//esperar n ticks
+    }
+
+    public int MuestrasTomadas
+    {
+        get { return nMuestras; }
+    }
+} 
